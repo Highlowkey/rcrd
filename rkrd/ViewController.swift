3a78dfe58d2d39cb5ed794dce663bb4a300c82cc
pmcelroy <pmcelroy@umich.edu>
2019-09-12 11:32:38 -0400

    Initial commit, with basic workingmain screen

--- rkrd/ViewController.swift
+++ rkrd/ViewController.swift
@@ -8,13 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var rkrdText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func add_rkrd(_ sender: Any) {
+        print("Hello");
+        self.view.viewWithTag(1)?.isHidden = false;
+    }
+    
+    @IBAction func done_adding(_ sender: Any) {
+        self.view.viewWithTag(1)?.isHidden = true;
+        print(rkrdText.text ?? "N/A")
+    }
 }
 
