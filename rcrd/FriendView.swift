4866596db1bc5230e9f7c5b66d1d38844f74fa19
pmcelroy <pdmcelroy1001@gmail.com>
2019-12-23 14:59:22 -0500

    lots of friend stuff

--- rcrd/FriendView.swift
+++ rcrd/FriendView.swift
@@ -15,11 +15,53 @@ class FriendView: UIViewController {
     
     var ref: DatabaseReference!
     var otherUser: String!
+    @IBOutlet weak var otherAccountName: UITextField!
+    var friends: [String] = []
+    @IBOutlet weak var friend: UIButton!
+    @IBOutlet weak var friendName: UILabel!
+    @IBOutlet weak var friendDescript: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         ref = Database.database().reference()
+        ref.observeSingleEvent(of: .value, with: {(snapshot) in
+            for friend in snapshot.childSnapshot(forPath: UIDevice.current.identifierForVendor!.uuidString).childSnapshot(forPath: "friends").children.allObjects as! [DataSnapshot] {
+                self.friends.append(friend.key)
+            }
+            if(self.friends.count > 0) {
+                self.friendName.text! = String(self.friends[0].prefix(1).uppercased())
+                self.friendDescript.text! = self.friends[0]
+                self.friendName.isHidden = false
+                self.friendDescript.isHidden = false
+                self.friend.isHidden = false
+            }
+        })
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func showFriend(_ sender: Any) {
+        otherRcrds.rcrds.removeAll()
+        ref.observeSingleEvent(of: .value, with: {(snapshot) in
+            self.otherUser = snapshot.childSnapshot(forPath: self.friends[0]).value as? String
+            for rcrd in snapshot.childSnapshot(forPath: self.otherUser).childSnapshot(forPath: "rcrds").children.allObjects as! [DataSnapshot] {
+                let rcrdName = rcrd.key
+                let rcrdType = rcrd.childSnapshot(forPath: "type").value as! String
+                let isFollowing = rcrd.childSnapshot(forPath: "following").value as! Bool
+                var rcrdValuesArray: [String] = []
+                if (rcrd.childSnapshot(forPath: "values").childrenCount > 0) {
+                    for value in rcrd.childSnapshot(forPath: "values").children.allObjects as! [DataSnapshot] {
+                        rcrdValuesArray.append(value.value as! String)
+                        }
+                }
+                otherRcrds.rcrds.append(Rcrd(rcrdName, rcrdValuesArray, isFollowing, rcrdType))
+            }
+            self.friendName.text! = String(self.friends[0].prefix(1).uppercased())
+            self.friendDescript.text! = self.friends[0]
+            self.friendName.isHidden = false
+            self.friendDescript.isHidden = false
+            self.friend.isHidden = false
+            self.performSegue(withIdentifier: "otherListSegue", sender: self)
+        })
     }
     
     @IBAction func addFriend(_ sender: Any) {
@@ -27,23 +69,15 @@ class FriendView: UIViewController {
     }
 
     @IBAction func showAccount(_ sender: Any) {
-//        otherRcrds.rcrds.removeAll()
-//        ref.observeSingleEvent(of: .value, with: {(snapshot) in
-//            self.otherUser = snapshot.childSnapshot(forPath: self.otherAccountName.text!).value as? String
-//            for rcrd in snapshot.childSnapshot(forPath: self.otherUser).childSnapshot(forPath: "rcrds").children.allObjects as! [DataSnapshot] {
-//                let rcrdName = rcrd.key
-//                let rcrdType = rcrd.childSnapshot(forPath: "type").value as! String
-//                let isFollowing = rcrd.childSnapshot(forPath: "following").value as! Bool
-//                var rcrdValuesArray: [String] = []
-//                if (rcrd.childSnapshot(forPath: "values").childrenCount > 0) {
-//                    for value in rcrd.childSnapshot(forPath: "values").children.allObjects as! [DataSnapshot] {
-//                        rcrdValuesArray.append(value.value as! String)
-//                        }
-//                }
-//                otherRcrds.rcrds.append(Rcrd(rcrdName, rcrdValuesArray, isFollowing, rcrdType))
-//            }
-//            self.performSegue(withIdentifier: "otherListSegue", sender: self)
-//        })
+        otherRcrds.rcrds.removeAll()
+    ref.child(UIDevice.current.identifierForVendor!.uuidString).child("friends").child(otherAccountName.text!)
+        friends.append(otherAccountName.text!)
+        self.friendName.text! = String(self.friends[0].prefix(1).uppercased())
+        self.friendDescript.text! = self.friends[0]
+        self.friendName.isHidden = false
+        self.friendDescript.isHidden = false
+        self.friend.isHidden = false
+        self.view.viewWithTag(1)?.isHidden = true
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
